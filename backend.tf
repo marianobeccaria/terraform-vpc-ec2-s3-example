4111adb423118142f34b2edf6e3e36ac97e6b8c9
Mariano B <mbeccaria@sent.com>
2023-05-28 14:24:28 -0500

    First commit terraform proj

---
+++ backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">=0.12.0"
+  required_providers {
+    aws = ">=3.0.0"
+  }
+  backend "s3" {
+    region  = "us-east-1"
+    profile = "terraform"
+    key     = "ec2s3example_state_file"
+    bucket  = "terraformstate-mariano-81726354"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "profile" {
+  type    = string
+  default = "terraform"
+}
+
+variable "region-master" {
+  type    = string
+  default = "us-east-1"
+}
